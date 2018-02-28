7129045071d7b5df1a48b4a84315e4f33af610bc
Kirill <kirill.tovpinets@gmail.com>
2018-02-28 16:17:24 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,118 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.value = initialValue;
+    this.result = 0;
+    this.exp = [];
   }
 
   add(number) {
-    // your implementation
+    this.exp.push('+');
+    this.exp.push(number)
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.exp.push('-');
+    this.exp.push(number)
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.exp.push('*');
+    this.exp.push(number)
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.exp.push('/');
+    this.exp.push(number)
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.exp.push('^');
+    this.exp.push(number)
+    return this;
+  }
+
+  valueOf(){
+    this.getValue('^');
+    this.getValue('*');
+    this.getValue('/');
+    this.getValue('+');
+    this.getValue('-');
+    this.result = this.exp[0];
+    return parseInt(this.result);
+  }
+  toString(){
+    this.getValue('^');
+    this.getValue('*');
+    this.getValue('/');
+    this.getValue('+');
+    this.getValue('-');
+    this.result = this.exp.join("");
+  }
+  getValue(operator){
+    var startIndex = 0;
+    while(true){
+      var mulIndex = this.exp.indexOf(operator, startIndex);
+      startIndex = mulIndex + 1;
+      
+      if (mulIndex !== -1) {
+        if (mulIndex === 0) {
+          switch(operator){
+            case '+':{
+              this.exp[mulIndex] = this.value + this.exp[mulIndex + 1];
+              break;
+            }
+            case '-':{
+              this.exp[mulIndex] = this.value - this.exp[mulIndex + 1];
+              break;
+            }
+            case '*':{
+              this.exp[mulIndex] = this.value * this.exp[mulIndex + 1];
+              break;
+            }
+            case '/':{
+              this.exp[mulIndex] = this.value / this.exp[mulIndex + 1];
+              break;
+            }
+            case '^':{
+              this.exp[mulIndex] = Math.pow(this.value, this.exp[mulIndex + 1]);
+              break;
+            }
+          }
+          this.exp.splice(mulIndex + 1, 1);
+        }else{
+          switch(operator){
+            case '+':{
+              this.exp[mulIndex - 1] = this.exp[mulIndex - 1] + this.exp[mulIndex + 1];
+              break;
+            }
+            case '-':{
+              this.exp[mulIndex - 1] = this.exp[mulIndex - 1] - this.exp[mulIndex + 1];
+              break;
+            }
+            case '*':{
+              this.exp[mulIndex - 1] = this.exp[mulIndex - 1] * this.exp[mulIndex + 1];
+              break;
+            }
+            case '/':{
+              this.exp[mulIndex - 1] = this.exp[mulIndex - 1] / this.exp[mulIndex + 1];
+              break;
+            }
+            case '^':{
+              this.exp[mulIndex - 1] = Math.pow(this.exp[mulIndex - 1], this.exp[mulIndex + 1]);
+              break;
+            }
+          }
+          this.exp.splice(mulIndex + 1, 1);
+          this.exp.splice(mulIndex, 1);
+        }
+      }else{
+        return;
+      }
+    }
   }
 }
 
